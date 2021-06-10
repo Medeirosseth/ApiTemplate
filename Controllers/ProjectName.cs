798c99c801f45994a76c5c38385bffd165215631
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-10 10:12:15 -0700

    add intial files and boiler plate

---
+++ Controllers/ProjectName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+////using ProjectName.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+// namespace ProjectName.Controllers
+// {
+//     [ApiController]
+//     [Route("api/[controller]")]
+
+// }
